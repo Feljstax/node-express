fc2445b2d097f97f777af6c1f05b4cb1c69ef887
Felj Abas <feljstax@gmail.com>
2022-12-10 00:05:27 -0800

    Introduction to Express Part 2

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-//setup server so it returns same response for any request
+//Configures Morgan to use development versin
+app.use(morgan('dev'));
 
+
+//Modify Express to serve files from public folder with middeware
+//__dirname is a special variable in Node. It refers to absolute path of current directory of the file it's in
+app.use(express.static(__dirname + '/public'));
+
+//setup server so it returns same response for any request
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
-    res.header('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
